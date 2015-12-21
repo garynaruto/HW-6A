6525915c523a301e88ac531ca740f9ced6d9eb9c
garynaruto <garynaruto@gmail.com>
2015-12-21 20:55:03 +0800

    v04

--- src/A6.java
+++ src/A6.java
@@ -4,43 +4,43 @@ import java.util.ArrayList;
 public class A6{
 	public static void main(String[] args){
 		Question title = new Question("Question 1");
-        
+		
 		QuestionComposite ch1 = new QuestionComposite();
-        ch1.add(new Question("A1"));
-        ch1.add(new Question("A2"));
-        
-        QuestionComposite ch2 = new QuestionComposite();
-        ch2.add(new Question("B1"));
-        ch2.add(new Question("B2"));
-        
-        QuestionComposite all = new QuestionComposite();
-        all.add(title);
-        all.add(ch1);
-        all.add(ch2);
-        
-        all.print();
-    }
+		ch1.add(new Question("A1"));
+		ch1.add(new Question("A2"));
+		
+		QuestionComposite ch2 = new QuestionComposite();
+		ch2.add(new Question("B1"));
+		ch2.add(new Question("B2"));
+		
+		QuestionComposite all = new QuestionComposite();
+		all.add(title);
+		all.add(ch1);
+		all.add(ch2);
+		
+		all.print();
+	}
 }
 interface Printable{
-    void  print();
+	void  print();
 }
 class Question implements Printable{
-    private String description;
-    Question(String s){
-        this.description = s;
-    }
-    public void print(){
-        System.out.println(description);
-    }
+	private String description;
+	Question(String s){
+		this.description = s;
+	}
+	public void print(){
+		System.out.println(description);
+	}
 }
 class QuestionComposite implements Printable{
-    private ArrayList<Printable> list = new ArrayList<Printable>();
-    public void add(Printable p) {
-        list.add(p);
-    }
-    public void print(){
-        for(Printable p : list) {
-            p.print();
-        }
-    }
+	private ArrayList<Printable> list = new ArrayList<Printable>();
+	public void add(Printable p) {
+		list.add(p);
+	}
+	public void print(){
+		for(Printable p : list) {
+			p.print();
+		}
+	}
 }
